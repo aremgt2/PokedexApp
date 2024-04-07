25b2a84f41a6d85c3c89491ac80ccf32f1ae14de
Arem Dogan <doganarem@hotmail.com>
2024-04-07 10:48:52 +0200

    PokemonApp

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ThemeProvider attribute="class" defaultTheme="dark">
+          <main className="flex min-h-screen flex-col items-center p-24">
+            <div className="z-10 max-w-5xl w-full items-center justify-between text-sm lg:flex">
+              <Link href="/">
+                <h2 className="text-2x1 text-bold">PokemonFinder</h2>
+              </Link>
+            </div>
+            {children}
+          </main>
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
